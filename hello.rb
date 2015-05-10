e41acbce04382e850f63d52f9ae63d464cc14940
sultan.kh30@gmail.com <sultankh@B4-FM3-C320-11.ACADEMY.JCE.AC.IL>
2015-05-10 16:10:44 +0300

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -9,3 +9,25 @@ puts "Hello,#{name}!"
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
+
+# This is an unwanted but staged comment
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+# This is an unwanted but committed change
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+# Default is World
+# Author: Jim Weirich
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+# Default is World
+# Author: Jim Weirich (sultan.kh30@gmail.com)
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
