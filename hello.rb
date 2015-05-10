dd69e732d21deeea8741525ba17363392afa0af0
sultan.kh30@gmail.com <sultankh@B4-FM3-C320-11.ACADEMY.JCE.AC.IL>
2015-05-10 16:14:57 +0300

    Add an author/email comment

--- hello.rb
+++ hello.rb
@@ -26,8 +26,21 @@ name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
 
+
 # Default is World
 # Author: Jim Weirich (sultan.kh30@gmail.com)
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+# Default is World
+# Author: Jim Weirich
+name = ARGV.first || "World"
+
+puts "Hello, #{name}!"
+
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
